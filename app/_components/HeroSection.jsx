ea633906a3bc818167c9b2ff27438b2930ebbbb1
Aditya Prasad <adityaprasad7123@gmail.com>
2024-05-10 18:34:57 -0400

    Changed type animation text

--- app/_components/HeroSection.jsx
+++ app/_components/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
         <section>
             <div className='grid grid-cols-1 lg:grid-cols-12'>
                 <div className='col-span-7 place-self-center text-center sm:text-left'>
-                    <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+                    <h1 className='text-white mb-4 text-4xl sm:text-5xl text-nowrap lg:text-6xl font-extrabold'>
                         <sapan className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
                             Hello, I&apos;m {" "}
                         </sapan>
@@ -20,7 +20,7 @@ const HeroSection = () => {
                             sequence={[
                                 'Aditya Prasad',
                                 1000,
-                                'A Web Developer',
+                                'Web Developer',
                                 2000,
                             ]}
                             wrapper="span"
